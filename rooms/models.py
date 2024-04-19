7e3648a3272fd83e69287f5113ca95238aa837ac
HyunJun Baek <hjbaek5796@gmail.com>
2024-04-19 21:11:22 +0900

    Create room admin and implement list and filters

--- rooms/models.py
+++ rooms/models.py
@@ -10,6 +10,10 @@ class Room(CommonModel):
     PRIVATE_ROOM = ("private_room", "Private Room")
     SHARED_ROOM = "shared_room", "Shared Room"
 
+  name = models.CharField(
+    max_length=180,
+    default="",
+  )
   country = models.CharField(
     max_length=50,
     default="S.Korea",
@@ -40,6 +44,9 @@ class Room(CommonModel):
     "rooms.Amenity",
   )
 
+  def __str__(self) -> str:
+    return self.name
+
 
 class Amenity(CommonModel):
 
@@ -51,5 +58,11 @@ class Amenity(CommonModel):
   description = models.CharField(
     max_length=150,
     null=True,
+    blank=True,
   )
 
+  def __str__(self) -> str:
+    return self.name
+
+  class Meta:
+    verbose_name_plural = "Amenities"
