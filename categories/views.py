6dd956b2db6f66ca19fe0ea8f28148a68e2d58b5
HyunJun Baek <hjbaek5796@gmail.com>
2024-04-25 21:14:44 +0900

    Implement GET method

--- categories/views.py
+++ categories/views.py
@@ -3,14 +3,16 @@ from rest_framework.response import Response
 from .models import Category
 from .serializers import CategorySerializer
 
+
 @api_view()
 def categories(request):
   all_categories = Category.objects.all()
   serializer = CategorySerializer(all_categories, many=True)
-  return Response(
-    {
-      "ok": True,
-      "categories": serializer.data,
-    }
-  )
+  return Response(serializer.data)
+
+@api_view()
+def category(request, pk):
+  category = Category.objects.get(pk=pk)
+  serializer = CategorySerializer(category)
+  return Response(serializer.data)
 
