b8c262d5b6d77e845b54d09f4ff16cd5cc001dfc
HyunJun Baek <hjbaek5796@gmail.com>
2024-04-25 21:29:41 +0900

    Implement POST method

--- categories/views.py
+++ categories/views.py
@@ -4,11 +4,19 @@ from .models import Category
 from .serializers import CategorySerializer
 
 
-@api_view()
+@api_view(["GET", "POST"])
 def categories(request):
-  all_categories = Category.objects.all()
-  serializer = CategorySerializer(all_categories, many=True)
-  return Response(serializer.data)
+
+  if request.method == "GET":
+    all_categories = Category.objects.all()
+    serializer = CategorySerializer(all_categories, many=True)
+    return Response(serializer.data)
+  elif request.method == "POST":
+    Category.objects.create(
+      name=request.data["name"],
+      kind=request.data["kind"],
+    )
+    return Response({"created": True})
 
 @api_view()
 def category(request, pk):
