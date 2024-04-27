3ca738c397c4c43e3aebae0c0eec96f43896b82b
HyunJun Baek <hjbaek5796@gmail.com>
2024-04-27 15:42:48 +0900

    [Amenities] Set up url paths for amenities

--- rooms/views.py
+++ rooms/views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+from rest_framework.views import APIView
+
+class Amenities(APIView):
+  
+  def get(self, request):
+    pass
+
+  def post(self, request):
+    pass
+
+class AmenityDetail(APIView):
+
+  def get(self, request, pk):
+    pass
+
+  def put(self, request, pk):
+    pass
