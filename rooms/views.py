e7c3d0cd67dd525064b861eaeb077d8fdc225bc8
HyunJun Baek <hjbaek5796@gmail.com>
2024-05-03 14:04:28 +0900

    [Room] Implement rollback if info is not right

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,5 @@
 from rest_framework.views import APIView
+from django.db import transaction
 from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound, NotAuthenticated, ParseError
@@ -73,20 +74,20 @@ class Rooms(APIView):
             raise ParseError("The category kind should be 'rooms'")
         except Category.DoesNotExist:
           raise ParseError("Category not found")
-        room = serializer.save(
-          owner=request.user,
-          category=category,
-        )
-        amenities = request.data.get("amenities")
-        for amenity_pk in amenities:
-          try:
-            amenity = Amenity.objects.get(pk=amenity_pk)
-          except Amenity.DoesNotExist:
-            room.delete()
-            raise ParseError(f"Amenity with id {amenity_pk} not found")
-          room.amenities.add(amenity)
-        serializer = RoomDetailSerializer(room)
-        return Response(serializer.data)
+        try:
+          with transaction.atomic():
+            room = serializer.save(
+              owner=request.user,
+              category=category,
+            )
+            amenities = request.data.get("amenities")
+            for amenity_pk in amenities:
+              amenity = Amenity.objects.get(pk=amenity_pk)
+              room.amenities.add(amenity)
+            serializer = RoomDetailSerializer(room)
+            return Response(serializer.data)
+        except Exception:
+          raise ParseError("Amenity not found")
       else:
         return Response(serializer.errors)
     else:
@@ -104,3 +105,22 @@ class RoomDetail(APIView):
     room = self.get_object(pk)
     serializer = RoomDetailSerializer(room)
     return Response(serializer.data)
+  
+
+
+'''
+{
+  "name": "House created with DRF",
+  "country": "S.Korea",
+  "city": "Seoul",
+  "price": 1000,
+  "rooms": 2,
+  "toilets": 2,
+  "description": "DRF is great!",
+  "address": "123",
+  "pet_friendly": true,
+  "category": 3,
+  "amenities": [67777, 2, 3, 4],
+  "kind": "private_room"
+}
+'''
